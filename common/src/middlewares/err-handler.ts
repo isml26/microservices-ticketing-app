4206b547a49dbab65baf4734fe25fb47144a85b8
ismail26 <iguven1999@hotmail.com>
2021-08-03 00:08:28 +0300

    Updates

--- common/src/middlewares/err-handler.ts
+++ common/src/middlewares/err-handler.ts
@@ -9,7 +9,7 @@ export const errorHandler:ErrorRequestHandler=(
     if(err instanceof CustomError){
       return res.status(err.statusCode).send({errors:err.serializeErrors()});
     }
-
+    console.log(err);
     res.status(400).send({
         errors:[{
           message:'Something went wrong'
